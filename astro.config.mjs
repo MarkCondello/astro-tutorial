96c8205fe9f46cd429aac29e839e96c1634fd8ff
Mark Condello <mark.condello@brightlabs.com.au>
2026-05-15 14:51:36 +1000

    - included preact island for greeting component - added theme color switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrotutorialbrightlabs.netlify.app"
+  site: "https://astrotutorialbrightlabs.netlify.app",
+  integrations: [preact()]
 });
